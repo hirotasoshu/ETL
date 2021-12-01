740e20cade55a87d594a3ce8ec62de3069db922c
Maksim Zayakin <rasetsu@list.ru>
2021-12-02 01:45:52 +0500

    :art: Refactor pg extractor for multiple indexes
    Also add genres indexes support

--- postgres_to_es/postgres_extractor.py
+++ postgres_to_es/postgres_extractor.py
@@ -1,4 +1,4 @@
-from typing import Iterator, Optional, Tuple
+from typing import Iterator, Optional, Tuple, Type
 
 import backoff
 import psycopg2
@@ -6,7 +6,7 @@ from psycopg2.extensions import connection as pg_conn
 from psycopg2.extras import DictCursor
 
 from config import BACKOFF_CONFIG, PostgresDsn
-from models import MoviesES
+from models import AbstractModel, GenresES, MoviesES
 
 
 class PostgresExtractor:
@@ -36,8 +36,8 @@ class PostgresExtractor:
         return psycopg2.connect(**self._dsn.dict(), cursor_factory=DictCursor)
 
     @backoff.on_exception(**BACKOFF_CONFIG)
-    def _extract_movies_data(
-        self, query: str, itersize: int
+    def _get_generator(
+        self, model: Type[AbstractModel], query: str, itersize: int
     ) -> Iterator[Tuple[dict, str]]:
         """Возвращает итератор данных в нужном формате для ES"""
         cur = self.postgres_connection.cursor()
@@ -45,18 +45,27 @@ class PostgresExtractor:
         cur.execute(query)
 
         for row in cur:
-            movie = MoviesES(**row).dict()
+            instance = model(**row).dict()
             # NOTE: костыль, чтобы айди в ES и PG совпадали
             # и работал поиск по id из пг
-            movie["_id"] = movie["id"]
-            yield movie, str(row["updated_at"])
+            instance["_id"] = instance["id"]
+            yield instance, str(row["updated_at"])
 
     def extract_data(
         self, index: str, query: str, itersize: int
     ) -> Iterator[Tuple[dict, str]]:
+
+        model = AbstractModel
+
         if index == "movies":
-            return self._extract_movies_data(query, itersize)
+            model = MoviesES
+
+        elif index == "genres":
+            model = GenresES
 
         # Так можно добавить другие индексы
+
+        if model != AbstractModel:
+            return self._get_generator(model, query, itersize)
 
         raise ValueError(f"No extract process for index {index}")
